66ae49d5ef31813b3e571f8571092c92b51df0d4
habib <habibdev96@gmail.com>
2021-09-19 20:53:35 -0600

    react router setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import Globals from './abstracts/Globals';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Pricing from './pages/Pricing';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 const App = () => {
   return (
     <>
       <Globals />
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/Pricing' component={Pricing} />
+          <Route exact path='/About' component={About} />
+          <Route exact path='/Contact' component={Contact} />
+        </Switch>
+      </Router>
     </>
   );
 };
